f3d41e90943e1b5596781a6a8d5dae1bb519fa45
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-09-06 09:33:25 +0200

    Init commit

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+//generate test
+//alt + enter
+public class App {
+
+    //psvm + tab
+    public static void main(String[] args) {
+        //sout + tab
+        System.out.println("test");
+    }
+}
