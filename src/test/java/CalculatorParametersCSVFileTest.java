a84b3b0ac3903abd90671e97c0e4eea92d3b1b88
Jakub Olszewski <j.b.olszewski@gmail.com>
2018-09-10 18:09:52 +0200

    Zadanie: Przed wykonaniem kazdego testu utworz instancje kalkulatora @Before wykonaj testy odejmowania mnozenia i dzielenia na podstawie przykladu ponizej utworz odpowiednie pliki CSV i zmodyfikuj sciezki w @FileParameters

--- src/test/java/CalculatorParametersCSVFileTest.java
+++ src/test/java/CalculatorParametersCSVFileTest.java
@@ -1,5 +1,6 @@
 import junitparams.FileParameters;
 import junitparams.JUnitParamsRunner;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -8,15 +9,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @RunWith(JUnitParamsRunner.class)
 public class CalculatorParametersCSVFileTest {
 
+    // zmienna/ pole dostepna w kazdej metodzie
+    Calculator calc;
+
     //przed wykonaniem kazdego testu utworz instancje kalkulatora @Before
     //wykonaj testy odejmowania mnozenia i dzielenia na podstawie przykladu ponizej
     //utworz odpowiednie pliki CSV i zmodyfikuj sciezki w @FileParameters
 
+    // metoda ponizej uruchamia sie przed kazdym testem czyli przed wykonaniem metody @Test
+    @Before
+    public void setUp(){
+
+        calc = new CalculatorImpl();
+
+        // zmienna ponizej dostepna tylko w bloku kodu {}
+        // Calculator calc = new CalculatorImpl();
+    }
+
     @Test
-    @FileParameters("src/test/resources/CalculatorParametersCSVFileTest.csv")
+    @FileParameters("src/test/resources/CalculatorParametersAddCSVFileTest.csv")
     public void addTest(double number1, double number2, String result) {
         calc.add(number1,number2);
         assertEquals(result, calc.display());
     }
 
+    @Test
+    @FileParameters("src/test/resources/CalculatorParametersSubCSVFileTest.csv")
+    public void subTest(double number1, double number2, String result) {
+        calc.add(number1,number2);
+        assertEquals(result, calc.display());
+    }
 }
